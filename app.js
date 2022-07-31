84b895203e201e8644abfc82330471f21d030ba6
Beginner-Project <boripats63@nu.ac.th>
2022-07-31 20:15:30 +0700

    codelab5

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 //app.js
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-
-var util = require('./utility');
-
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+var bodyParser = require('body-parser');
+var util = require("./utility");
 
 var app = express();
+var bp = bodyParser.urlencoded({extended: false});
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,46 +15,73 @@ app.use(express.urlencoded({ extended: true }));
  Create my-route
 **/
 
-app.get('/', function(req, res) {
-    res.sendFile('public/index.html', { root: __dirname });
+// app.get('/', function(req, res) {
+//     res.sendFile('public/index.html', { root: __dirname });
+// });
+
+app.post("/on_submit", function (req, res) {
+  console.log(req.body);
 });
 
-app.get('/welcome', (req, res) =>
+app.get("/welcome", (req, res) =>
   res.send({
     error: false,
-    message: 'Welcome NodeJS, Express',
-  }),
+    message: "Welcome NodeJS, Express",
+  })
 );
 
-app.get("/form.html",(req,res)=>{
-    res.sendFile(path.join(__dirname+'/public/form.html'));
+app.get("/form.html", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/form.html"));
 });
-app.post('/', function(req, res) {
-   var result ={ student_id: req.body.student_id,
-     firstname: req.body.firstname,
-     lastname: req.body.lastname,
-     gender: req.body.gender
-   }
+
+app.get("/login.html",(req, res) => {
+  res.sendFile(path.join(__dirname + "/public/login.html"));
+});
+
+app.post("/", function (req, res) {
+  var result = {
+    student_id: req.body.student_id,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    gender: req.body.gender,
+  };
   res.json(result);
 });
-app.get('/release', (req, res) =>{
-   res.status(400).send("error aria?");
+app.get("/release", (req, res) => {
+  res.status(400).send("error aria?");
 });
 
-app.get('/ok', (req, res) =>{
-   res.status(200).json({ "status": true, "result": ' successful!' });
+app.get("/ok", (req, res) => {
+  res.status(200).json({ status: true, result: " successful!" });
 });
 
 
-app.get('/student/:student_id', function(req, res) {
-    util.fakeStudentbyInfo(req.params.student_id, function (result) {
-        res.json(result);
-        console.log(result);
-    });
+app.post("/on_login", function (req, res) {
+  var email = req.body.txtEmail;
+  var password = req.body.txtPassword;
 });
 
+app.get("/student/:student_id", function (req, res) {
+  util.findStudentbyId(req.params.student_id, function (result) {
+    // res.json(result);
+    // console.log(result);
+    res.send(result);
+  });
+});
+
+/** specify the directory from where to serve static assets such as JavaScript, CSS, images **/
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist/"));
+app.use(
+  "/jquery-ui",
+  express.static(__dirname + "/node_modules/jquery-ui/dist/")
+);
+
+/** remove fix route and use path solution **/
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-    console.log('Starting node.js on port ' + port);
+  console.log("Starting node.js on port " + port);
 });
--- utility.js
+++ utility.js
@@ -1,8 +1,8 @@
 //utility.js
 
 const request = require("request-promise");
-const EXTERNAL_API=""; // put url
-const accessToken=""; // put access token
+const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MjYwNTk4LCJleHAiOjE2NTkyNjQxOTgsIm5iZiI6MTY1OTI2MDU5OCwianRpIjoiWkpDamJxN1VHMmlFQ3c0TSIsInN1YiI6MTEzLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.QlHUi00qft8TE6ePJoN7ZA-jQxc5oORpgzlmLMHOcUg"; // put access token
 const student = {
     student_id: 63363522,
     name: '', 
