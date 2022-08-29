ec7ce813071fcb9c578538be711503540b182213
Beginner-Project <boripats63@nu.ac.th>
2022-08-29 20:29:10 +0700

    add all

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MjYwNTk4LCJleHAiOjE2NTkyNjQxOTgsIm5iZiI6MTY1OTI2MDU5OCwianRpIjoiWkpDamJxN1VHMmlFQ3c0TSIsInN1YiI6MTEzLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.QlHUi00qft8TE6ePJoN7ZA-jQxc5oORpgzlmLMHOcUg"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzc4OTQ3LCJleHAiOjE2NjE3ODI1NDcsIm5iZiI6MTY2MTc3ODk0NywianRpIjoiZ28zTTZCbWNJVkwwWDVYciIsInN1YiI6MTEzLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.wxGqk89f3FA9-ZaYdS-7pTftmSAsy2O-tCV8KRGkq7o"; // put access token
 const student = {
     student_id: 63363522,
     name: '', 
@@ -16,7 +16,7 @@ exports.findStudentbyId = function (student_id, cb) {
   //-- call external api 
           request({
             method: "POST",
-            uri: EXTERNAL_API,
+            uri: EXTERNAL_API+student_id,
             headers: {
                 Authorization: `Bearer ${accessToken}`
             },
